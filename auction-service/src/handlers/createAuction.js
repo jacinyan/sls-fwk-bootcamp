e6a4289ff5e22b366f7ea5bd82623ec7c751c0d8
Ji Yan <jiyan@Jis-MacBook-Air.local>
2021-10-30 21:46:18 +1100

    Test rsa

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -9,6 +9,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
+  //
   const now = new Date();
 
   const auction = {
